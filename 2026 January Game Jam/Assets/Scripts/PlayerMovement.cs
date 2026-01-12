4204498ff58fa36e72d835de5960a823b833292a
Linus Riddle <linusriddler@gmail.com>
2026-01-12 08:37:27 -0500

    Added Player object

---
+++ 2026 January Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public Rigidbody rb;
+    public float horizontalInput;
+    public float verticalInput;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
