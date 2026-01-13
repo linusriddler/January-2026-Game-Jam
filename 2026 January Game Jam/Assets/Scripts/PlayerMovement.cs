65b96d9c0038437277928bb6e63d4ef6a905cc9b
Linus Riddle <linusriddler@gmail.com>
2026-01-13 07:53:30 -0500

    Added nonfunctional jump

--- 2026 January Game Jam/Assets/Scripts/PlayerMovement.cs
+++ 2026 January Game Jam/Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,41 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1.0f;
+    public float jumpStrength = 1.0f;
     public Rigidbody rb;
-    public float horizontalInput;
-    public float verticalInput;
+    private float horizontalInput;
+    private float verticalInput;
+    private bool isGrounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
